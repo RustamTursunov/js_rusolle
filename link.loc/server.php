86aa6064803f571a78391e69e703e68189d935e2
RustamTursunov <58801659+RustamTursunov@users.noreply.github.com>
2020-05-27 15:39:24 +0300

    Update server.php

--- link.loc/server.php
+++ link.loc/server.php
@@ -1,4 +1,5 @@
 <?php 
+error_reporting(-1);
 
 if (isset($_POST['submit_request'])) {
 	$query = trim($_POST['query_link']);	    
@@ -6,39 +7,53 @@ if (isset($_POST['submit_request'])) {
     $query_link[2] = 'yourdomain';
     $query_link[3] = 'abCdE';
     $query_link = implode("/",array_splice($query_link, 0, 4) );
-    $res = '<a href="'.$_POST['query_link'].'" class="result_link" target="blank">'.$query_link.'</a>'; 
-   
-    $arr = [
-       "date" => date('Y-m-d H:i:s').'|',
-       "link_data" => $res.'|',
-       "clicked" => "Переходов:"       
+    $res = '<a href="'.$_POST['query_link'].'" class="result_link" target="blank">'.$query_link.'</a>';
+    $arr = [       
+       "link_data" => $res,
+       "clicked" => "Переходов: |".intval(0)."\n"      
     ];
 
     echo json_encode($arr);
     
-    $file = fopen("requests.txt", "a");
-    fwrite($file, implode(" ", $arr)."\n=================\n");
-    fclose($file);    
+    $file = fopen("requests.txt", "a+");
+    fwrite($file, implode("|", $arr));
+    fclose($file);
+     
     exit;
 }
 
 if (isset($_POST['clicked'])) {
-    $query = $_POST['clicked'];
-    $st_strpos = $query." Переходов:";
-    $filename = 'requests.txt';
-    $file = file_get_contents($filename);    
-    $all = 1;
-
-   foreach (explode('|', $file) as $value) {
-   	
-
-if (strpos($value, $st_strpos) == TRUE) {
-	$text = str_replace("Переходов:", '', "Переходов: ".++$all, $line);  
-	file_put_contents($filename, $text);
-    }
- }
+    $query = $_POST['clicked'];    
+    $query = htmlentities($query);  
+    $filename = "requests.txt";
+    $file = htmlentities(file_get_contents($filename));
+    $array = explode("\n", $file); 
+    $arr_rm_last = array_pop($array);
+    
+    foreach ($array as $arr) {	
+       	$result = explode("|", $arr);             
+    
+       	 	if (in_array($query, $result)){
+       			$result[2] = (int)$result[2] + 1;       		
+       	    }
   
+       	 $arr = implode("|", $result);
+       	    $arr = $arr."\n";
+       } 
+       
+       file_put_contents($filename, html_entity_decode($arr));
+
+       exit;
 }
 
+ $filename = "requests.txt";
+ $file = htmlentities(file_get_contents($filename));
 
+ debug($file);
+
+ function debug($data)
+{
+	echo '<pre>' .print_r($data, 1). '</pre>';
+}
+       
 ?>
